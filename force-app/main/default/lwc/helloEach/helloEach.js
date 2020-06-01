b5034d258d95334e2196e5767502c5d8b1f70798
Sarveswara rao <rao.sarveswara@gmail.com>
2020-06-01 23:12:30 +1000

    new

---
+++ force-app/main/default/lwc/helloEach/helloEach.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloEach extends LightningElement {
+    contacts=[
+        {
+            Id:1,
+            Name:'Amy',
+            Title:'VP'
+        },
+        {
+            Id:2,
+            Name:'Rao',
+            Title:'VP'
+        },
+        {
+            Id:3,
+            Name:'Raol',
+            Title:'VPP'
+        }
+    ]
+
+}
