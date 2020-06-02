a00899629a6530a6298b09a758d23d30ed456f94
Sarveswara rao <rao.sarveswara@gmail.com>
2020-06-02 17:16:56 +1000

    new component

---
+++ force-app/main/default/lwc/shapeCalculator/shapeCalculator.js
@@ -0,0 +1,44 @@
+import { LightningElement,track } from 'lwc';
+
+export default class ShapeCalculator extends LightningElement {
+    @track currentOutPut;
+    width;
+    height;
+    side;
+    diagonal1;
+    diagonal2;
+    sideChangeHandler(event)
+    {
+        this.side=event.target.value;
+    }
+    widthChangeHandler(event)
+    {
+        this.width=event.target.value;
+    }
+    heightChangeHandler(event)
+    {
+        this.height=event.target.value;
+    }
+    diagonal1ChangeHandler(event)
+    {
+        this.diagonal1=event.target.value;
+    }
+    diagonal2ChangeHandler(event)
+    {
+        this.diagonal2=event.target.value;
+    }
+    calculateSqAreaHandler(){
+        const s=parseInt(this.side);
+        this.currentOutPut='Area of the Square is: '+(s*s);
+    }
+    calculateRecAreaHandler(){
+        const w=parseInt(this.width);
+        const h=parseInt(this.height);
+        this.currentOutPut='Area of the Rectangle is: '+(w*h);
+    }
+    calculateRhAreaHandler(){
+        const d1=parseInt(this.diagonal1);
+        const d2=parseInt(this.diagonal2);
+        this.currentOutPut='Area of the rhombus is: '+(d1*d2)/2;
+    }
+}
