79f6322ac77a61c1336274226c0c82d8655182f0
Sarveswara rao <rao.sarveswara@gmail.com>
2020-06-03 16:13:15 +1000

    new

---
+++ force-app/main/default/lwc/employeeDetails/employeeDetails.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class EmployeeDetails extends LightningElement {
+    empDetails=[
+        {
+            empName:'steve',empAddress:'NC'
+        },
+        {
+            empName:'rao',empAddress:'NA'
+        },
+        {
+            empName:'aneesh',empAddress:'NT'
+        },
+        {
+            empName:'steven',empAddress:'NU'
+        }
+    ]
+}
