bb0fa7ee00df2d2bc3796cf79dc051e5d1c5f68c
Sarveswara rao <rao.sarveswara@gmail.com>
2020-06-05 14:47:47 +1000

    new

---
+++ force-app/main/default/lwc/childComp/childComp.js
@@ -0,0 +1,10 @@
+import { LightningElement,track,api } from 'lwc';
+
+export default class ChildComp extends LightningElement {
+    @track message;
+
+    @api
+    changeMessage(strString){
+       this.message=strString.toUpperCase();
+    }
+}
